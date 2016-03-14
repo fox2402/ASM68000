794866418be81cfe8ea8f83d5e53d25b0da4db2a
Fabien Houang <fabien.houang@epita.fr>
2016-03-14 12:20:54 +0100

    Soutenance 1

--- file.asm
+++ file.asm
@@ -1,14 +1,5 @@
-<<<<<<< HEAD
-
-  org $500
-main 
-
-  move #5,d0 ;erwteg
-illegal
-=======
   org $500
 main 
 
   move.l #5,d0 ;erwteg
   illegal
->>>>>>> 5de128cc0d4154f0e335a51ae94f355cf00e072d
