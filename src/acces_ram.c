78124c1703bfd996b1f57485891ba9bf6ca9833e
Tristan Ruter-Naon <tristan.ruter-naon@epita.fr>
2016-05-22 18:55:03 +0200

    ram access

---
+++ src/acces_ram.c
@@ -0,0 +1,34 @@
+uint32_t ram_read(uint32_t mask, int add)
+{
+  struct cpu* k = get_cpu();
+  int i =0;
+  uint32_t ret = 0;
+  while (i < 3)
+  {
+    ret = (uint32_t)(k->RAM[add]);
+    if (i != 2)
+    {
+      ret = (ret << 8);
+      add++;
+    }
+    i++;
+  }
+  return (ret & mask);
+}
+
+void ram_write(uint32_t mask, int add, uint32_t new)
+{
+  struct cpu* k = get_cpu();
+  int i =0;
+  uint32_t ret = (ram_read(!mask, add) || (new & mask));
+  while (i <= 3)
+  {
+    k->RAM[add] = (char)ret;
+    if (i != 3)
+    {
+      ret = (ret >> 8);
+      add++;
+    }
+    i++;
+  }
+}
